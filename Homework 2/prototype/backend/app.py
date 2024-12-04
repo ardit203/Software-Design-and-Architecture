493c2f81a4cb2d8d20946d2a7b22e186884a5319
ardit203 <116848141+ardit203@users.noreply.github.com>
2024-12-04 16:42:49 +0100

    Add files via upload

---
+++ Homework 2/prototype/backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/issuers', methods=['GET'])
+def get_issuers():
+    # Load the Excel file and read the issuers from it
+    df = pd.read_excel("issuers.xlsx")
+    issuers = df['Code'].tolist()  # Assuming column is 'issuer_name'
+    print(issuers)
+    return jsonify({'issuers': issuers})
+
+@app.route('/api/issuer-data', methods=['POST'])
+def get_issuer_data():
+    issuer = request.json.get('issuer')
+    print(issuer)
+    if issuer:
+        # Load data for the selected issuer
+        df = pd.read_excel(f"database/{issuer}.xlsx")
+        df = df.iloc[:100]
+        data = df.to_dict(orient='records')
+
+        return jsonify(data)
+    return jsonify({'error': 'Issuer not found'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
